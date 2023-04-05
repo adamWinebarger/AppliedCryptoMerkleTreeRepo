566d09ac0f5c704ca65a87a0065c9140c4f58cd2
adamWinebarger <50574306+adamWinebarger@users.noreply.github.com>
2023-04-05 12:17:09 -0400

    Added MerkleHashTree2: which is a C# version of our Merkle Hash Tree program that makes use of IO and Crypto libraries available in .NET

---
+++ MerkleHashTree2/MerkleHashTree2/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MerkleHashTree2
+{
+    public class Node
+    {
+        int key;
+        string value;
+        Node left, right;
+        bool isCopied;
+
+        public Node(string value, Node left = null, Node right = null, bool isCopied = false)
+        {
+            this.key = key;
+            this.value = value;
+            this.left = left;
+            this.right = right;
+            this.isCopied = isCopied;
+        }
+    }
+}
---
+++ MerkleHashTree2/MerkleHashTree2/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Note that this will not work on anything *NIX-based as I'm using some things unique to windows/NTFS
+using System.IO;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Console;
+
+SHA1 sha1Hash = SHA1.Create();
+int fileCount = Directory.GetFiles("Files\\", "*").Length; //number of files in the folder we'll be using
+
+WriteLine(fileCount);
+
+List<String> plaintextFiles = new();
+
+string hash(string str)
+{
+    byte[] hashBytes = sha1Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+    string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+    return hash;
+}
+
+string rehash(List<String> input)
+{
+    List<String> newStuff = new();
+
+    for (int i = 0; i < input.Count; i += 2)
+    {
+        string newHash = hash(input[i] + input[i + 1]);
+        newStuff.Add(newHash);
+    }
+
+    if (newStuff.Count % 2 != 0 && newStuff.Count != 1)
+        newStuff.Add(newStuff[newStuff.Count - 1]);
+
+    return (newStuff.Count == 1) ? newStuff[0] : rehash(newStuff);
+}
+
+//Driver Code
+
+foreach (var file in Directory.GetFiles("Files\\", "*"))
+{
+    string st = File.ReadAllText(file);
+    //WriteLine(st);
+    plaintextFiles.Add(st);
+}
+
+if (plaintextFiles.Count % 2 != 0)
+{
+    plaintextFiles.Add(plaintextFiles[plaintextFiles.Count - 1]);
+}
+
+List<String> ctf = new();
+
+
+foreach (var str in plaintextFiles)
+    ctf.Add(hash(str));
+
+
+
+//foreach (var b in ctf)
+//{
+//    WriteLine(b);
+//}
+
+string output = rehash(ctf);
+
+WriteLine(output);
